b1be7e12627f1b5dc996873f04ba8e5d8bf22f6e
w4sabi <jp.figueredo8@gmail.com>
2022-08-10 22:35:50 -0300

    PROBLEMA: Agora vamos implementar uma nova funcionalidade, Desconto Extra. Porém, como vamos gerenciar estados de uma determinada situação? Segue o commit com o problema. No próximo, teremos a solução.

--- src/main/java/orcamento/Orcamento.java
+++ src/main/java/orcamento/Orcamento.java
@@ -5,11 +5,26 @@ import java.math.BigDecimal;
 public class Orcamento {
     private BigDecimal valor;
     private int quantidadeItens;
+    private String situacao;
 
 
-    public Orcamento (BigDecimal valor, int quantidadeItens){
+    public Orcamento(BigDecimal valor, int quantidadeItens) {
         this.valor = valor;
         this.quantidadeItens = quantidadeItens;
+    }
+
+    public void aplicarDescontoExtra() {
+        BigDecimal valorDeDescontoExtra = BigDecimal.ZERO;
+        if (situacao.equals("EM ANALISE")) {
+            valorDeDescontoExtra = new BigDecimal("0.05");
+        } else if(situacao.equals("APROVADO")){
+            valorDeDescontoExtra = new BigDecimal("0.02");
+        }
+        this.valor = this.valor.subtract(valorDeDescontoExtra);
+    }
+
+    public void aprovar(){
+        this.situacao = "APROVADO";
     }
 
     public int getQuantidadeItens() {
